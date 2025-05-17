f13bab5085387ea4f224ba101308e24365a510ca
Luiz-Neto18 <luizneto23ago@gmail.com>
2025-05-16 21:38:06 -0300

    Passo a Passo para Commit

---
+++ passo-a-passo-commit.py
@@ -0,0 +1,20 @@
+#tu sempre que entrar aqui vai ter que seguir esses passo:
+
+#O primeiro passo sempre vai ser, mas, isso sóo vai ser quando tu abrir ou quando estivermos fazendo alterações
+# ao mesmo tempo: git pull
+#ele vai servir para puxar todas as atualizações do sistemas, ou seja, tu vai fazer isso todas as vezes que
+#entrar no sistema
+
+#depois de tu fazer as alterações tu vai fazer isso:
+
+#git add .
+#esse serve para dizer que tu tá adionando uma nova alteração
+
+#git commit -m "seu comentario"
+#esse serve para você comentar para dizer o que você alterou no código
+
+#git push
+#esse serve para tu enviar as alterações para o github
+
+#lembre que tudo isso deve ser escrito no terminal: Ver>Terminal
+
